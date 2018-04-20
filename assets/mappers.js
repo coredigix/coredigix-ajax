dc76d49584b84bbd20d8f9666b484d899040d1c8
rafikalid <khalid.rfk@gmail.com>
2018-04-20 18:44:10 +0100

    cc

--- assets/mappers.js
+++
@@ -1,8 +0,0 @@
-const MIMETYPE_MAP = {
-	json		: 'application/json',
-	xml			: 'application/xml',
-	urlEncoded	: 'application/x-www-form-urlencoded',
-	text		: 'text/plain',
-	string		: 'text/plain',
-	multipart	: ''
-};
